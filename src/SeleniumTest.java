6b53ea17677b3bb499eb7bae8648d55367f51739
Siska <siska@fazzfinancial.com>
2023-06-02 06:56:27 +0700

    verify user can filter product from highest price to lowest price

--- src/SeleniumTest.java
+++ src/SeleniumTest.java
@@ -9,6 +9,7 @@ class SeleniumTest2 {
     public static <FirefoxDriver> void main(String[] args){
         WebDriver driver=new ChromeDriver();
 
+        //Verify user can login
         driver.get("https://www.saucedemo.com/");
         WebElement username=driver.findElement(By.xpath("//input[@placeholder='Username']"));
         username.sendKeys("standard_user");
@@ -22,5 +23,17 @@ class SeleniumTest2 {
         Assert.isTrue(driver
                     .findElement(By.xpath("//div[@class='app_logo']"))
                     .isDisplayed(),"app_logo not found");
+
+        WebElement productFilter=driver.findElement(By.xpath("//select[@class='product_sort_container']"));
+        productFilter.click();
+
+        //Verify user can filter product from highest price to lowest price
+        WebElement optionHighPriceToLowPrice=driver.findElement(By.xpath("//option[@value='hilo']"));
+        optionHighPriceToLowPrice.click();
+
+        String highestProduct=driver.findElement(By.xpath("//div[@class='inventory_item'][1]/div[2]/div[2]/div")).getText();
+
+        Assert.isTrue(highestProduct.equals("$49.99"),"At the top of the product is not the highest price");
+
     }
 }
