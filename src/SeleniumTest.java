8e35ecb14dd5fa48ca49ac0b977725d5c55d0a0b
Siska <siska@fazzfinancial.com>
2023-06-02 06:09:03 +0700

    verify that user can login

--- src/SeleniumTest.java
+++ src/SeleniumTest.java
@@ -9,18 +9,18 @@ class SeleniumTest2 {
     public static <FirefoxDriver> void main(String[] args){
         WebDriver driver=new ChromeDriver();
 
-        driver.get("https://demo.guru99.com/");
-        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
-        element.sendKeys("abc@gmail.com");
+        driver.get("https://www.saucedemo.com/");
+        WebElement username=driver.findElement(By.xpath("//input[@placeholder='Username']"));
+        username.sendKeys("standard_user");
 
-        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
-        button.click();
-        try {
-            Assert.isTrue(driver
-                    .findElement(By.xpath("//h2[@style='color:#800080;font-size:28px;font-weight:700;']"))
-                    .isDisplayed(), "Element //h2[@style='color:#800080;font-size:28px;font-weight:700; not found");
-        }catch(Exception e){
-            System.out.println("Assertion Error");
-        }
+        WebElement password=driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        password.sendKeys("secret_sauce");
+
+        WebElement login=driver.findElement(By.xpath("//input[@name='login-button']"));
+        login.click();
+
+        Assert.isTrue(driver
+                    .findElement(By.xpath("//div[@class='app_logo']"))
+                    .isDisplayed(),"app_logo not found");
     }
 }
